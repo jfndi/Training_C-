e6555a8be2dbfc07a95d6be9389ea313408eafc0
jfndi <143571177+jfndi@users.noreply.github.com>
2023-08-31 20:51:56 +0200

    Previous version does not work in all cases. This one passes all the test cases.

--- Deque-STL2/deque2.cpp
+++ Deque-STL2/deque2.cpp
@@ -3,49 +3,27 @@
 
 using namespace std;
 
-struct MaxDescriptor
-{
-    int index;
-    int value;
-};
-
 void printKMax(int arr[], int n, int k) {
     //Write your code here.
-    deque<MaxDescriptor> index_window_max{};
+    deque<int> index_window_max;
     int i = 0;
 
-    MaxDescriptor md{};
     for (i = 0; i < k; i++) {
-        if (md.value < arr[i])
-        {
-            md.index = i;
-            md.value = arr[i];
-            if (!index_window_max.empty())
-                index_window_max.pop_back();
-            index_window_max.push_back(md);
-        }
-    }
-
-    int start_index = 1;
-    for (; i < n; i++)
-    {
-        cout << arr[index_window_max.front().index] << " ";
-
-        if (index_window_max.front().index < start_index)
+        while ((!index_window_max.empty()) && arr[i] >= arr[index_window_max.back()])
             index_window_max.pop_back();
-
-        if (index_window_max.empty() || index_window_max.front().value < arr[i])
-        {
-            md.index = i;
-            md.value = arr[i];
-            if (!index_window_max.empty())
-                index_window_max.pop_back();
-            index_window_max.push_back(md);
-        }
-        start_index++;
+        index_window_max.push_back(i);
     }
 
-    cout << arr[index_window_max.front().index] << endl;
+    for (int j = i; j < n; ++j) {
+        cout << arr[index_window_max.front()] << " ";
+        while ((!index_window_max.empty()) && index_window_max.front() <= j - k)
+            index_window_max.pop_front();
+
+        while ((!index_window_max.empty() && arr[j] >= arr[index_window_max.back()]))
+            index_window_max.pop_back();
+        index_window_max.push_back(j);
+    }
+    cout << arr[index_window_max.front()] << endl;
 }
 
 int main() {
