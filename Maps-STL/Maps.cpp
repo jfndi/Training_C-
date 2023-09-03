2a2cc6789d827c2a159a2f5cb5a9acc69dd1aa03
jfndi <143571177+jfndi@users.noreply.github.com>
2023-09-03 09:28:47 +0200

    Ad Maps-STL solution

--- Maps-STL/Maps.cpp
+++ Maps-STL/Maps.cpp
@@ -10,5 +10,46 @@ using namespace std;
 
 int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+
+    int number_queries{};
+    cin >> number_queries;
+
+    map<string, int> marks_map{};
+    for (int i = 0; i < number_queries; i++)
+    {
+        int type{};
+        cin >> type;
+
+        string name{};
+        cin >> name;
+        switch (type)
+        {
+        case 1:
+        {
+            int mark{};
+            cin >> mark;
+            if (marks_map.find(name) == marks_map.end())
+                marks_map.insert(make_pair(name, mark));
+            else
+                marks_map[name] += mark;
+            break;
+        }
+        case 2:
+        {
+            marks_map.erase(name);
+            break;
+        }
+        case 3:
+        {
+            if (marks_map.find(name) == marks_map.end())
+                cout << 0;
+            else
+                cout << marks_map[name];
+            cout << endl;
+            break;
+        }
+        }
+    }
+
     return 0;
 }
