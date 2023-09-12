c22f0c27a9d824aa37e1f857579b59e6de0da57b
jfndi <143571177+jfndi@users.noreply.github.com>
2023-09-12 16:06:26 +0200

    Add MessageOrder project.

---
+++ MessageOrder/MessageOrder.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Message {
+private:
+    static int msg_num_;
+    int message_num_;
+    string message_{};
+
+public:
+    Message()
+    {
+        message_num_ = msg_num_++;
+    }
+
+    Message(const string& msg) : message_{msg}
+    {
+        message_num_ = msg_num_++;
+    }
+
+    const string& get_text() const {
+        return message_;
+    }
+
+    void set_text(string& text)
+    {
+        message_ = text;
+    }
+
+    friend bool operator<(const Message& msg1, const Message& msg2)
+    {
+        if (msg1.message_num_ < msg2.message_num_)
+            return true;
+        return false;
+    }
+};
+
+int Message::msg_num_ = 1;
+
+class MessageFactory {
+public:
+    MessageFactory() {}
+    Message create_message(const string& text) {
+        return Message(text);
+    }
+};
+
+class Recipient {
+public:
+    Recipient() {}
+    void receive(const Message& msg) {
+        messages_.push_back(msg);
+    }
+    void print_messages() {
+        fix_order();
+        for (auto& msg : messages_) {
+            cout << msg.get_text() << endl;
+        }
+        messages_.clear();
+    }
+private:
+    void fix_order() {
+        sort(messages_.begin(), messages_.end());
+    }
+    vector<Message> messages_;
+};
+
+class Network {
+public:
+    static void send_messages(vector<Message> messages, Recipient& recipient) {
+        // simulates the unpredictable network, where sent messages might arrive in unspecified order
+        random_shuffle(messages.begin(), messages.end());
+        for (auto msg : messages) {
+            recipient.receive(msg);
+        }
+    }
+};
+
+
+
+int main() {
+    MessageFactory message_factory;
+    Recipient recipient;
+    vector<Message> messages;
+    string text;
+    while (getline(cin, text)) {
+        messages.push_back(message_factory.create_message(text));
+    }
+    Network::send_messages(messages, recipient);
+    recipient.print_messages();
+}
